e4bebf2a17ac17fa3400aebab46c2f911a85211d
Harshita Bambure <harshitabambure@gmail.com>
2021-10-06 22:18:00 +0530

    database

--- app/src/main/java/com/example/roomdatabaseproject/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabaseproject/MainActivity.kt
@@ -25,15 +25,36 @@ class MainActivity : AppCompatActivity() {
         adapter = RecyclerAdapter()
         recyclerView!!.adapter = adapter
 
-        db= UserDatabase.getDatabase(this)!!
-        val user1 = User("harshu", "bambure","https://openai.com/content/images/2021/01/2x-no-mark-1.jpg")
-        val user2 = User("harshita", "xyz","https://openai.com/content/images/2021/01/2x-no-mark-1.jpg")
-        val user3 = User("minu", "cutu","https://openai.com/content/images/2021/01/2x-no-mark-1.jpg")
-        val user4 = User("rv", "qwerty","https://openai.com/content/images/2021/01/2x-no-mark-1.jpg")
+        db = UserDatabase.getDatabase(this)!!
+
+        val user1 =
+            User("Pink Bag", "$20", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0sGeKNQngI4Ou_4xoLkM8V_SEHnBZwYMshg&usqp=CAU")
+        val user2 =
+            User("Bag Black", "$30", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlTwVdQhinpCJoJSq7ErtO8XSLZXKWX4FrNw&usqp=CAU")
+        val user3 =
+            User("Red Bag", "$10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Q8UbOq6OZzvZBdtURmc3JC-7aQGqXnyWCg&usqp=CAU")
+        val user4 =
+            User("Bag Pink", "$15", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJgLEJqFbZbUGqV23KY7XTXcxLpxBZhYs-2w&usqp=CAU")
+        val user5 =
+            User("New Bag", "$17", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfTQwInYeHWNR-3Kwb1337U1GWItol1sI5Vw&usqp=CAU")
+        val user6 =
+            User("My Bag", "$24", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwQ0syEOfQ8TnEUczI1Xmy8Tc4lwsSkGDyRA&usqp=CAU")
+        val user7 =
+            User("Bag b", "$23", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDUgVcWDOUxkEb0fOrxenG2o_ujljbUaS3aQ&usqp=CAU")
+        val user8 =
+            User("Bags", "$20", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgvk9zW-IGR1kFjdtWGp6CG6oZtlStGJnDHw&usqp=CAU")
+        val user9 =
+            User("shopping Bag", "$14", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2YwDgCT83wfikBrI9mMhX4YItHxVNy5t26w&usqp=CAU")
+        val user10 =
+            User("bags", "$43", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu-PsgBuoFAaL_DuAbwjNN3YZ8U0YQy8NGeA&usqp=CAU")
+        val user11 =
+            User("my bags", "$23", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb791D7qVA3F9QL25FtCCHqC-GDR6uIRE7Ew&usqp=CAU")
+        val user12 =
+            User("New Bags  ", "$31", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNhGtr7RE2Y3INqiKbn6qz-CC4lUSeVsUyew&usqp=CAU")
 
         GlobalScope.launch {
             db.UserDao().deleteAll()
-            db.UserDao().insert(user1, user2, user3, user4)
+            db.UserDao().insert(user1, user2, user3, user4,user5,user6,user7,user8,user9,user10,user11,user12)
             displayUsers()
         }
 
